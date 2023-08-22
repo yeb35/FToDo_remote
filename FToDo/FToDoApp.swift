fac1f30e3577f579dfb67434d90782854017e05c
yeb35 <yunus.bolgonul@gmail.com>
2023-08-22 17:16:56 +0300

    Initial Commit

---
+++ FToDo/FToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FToDoApp.swift
+//  FToDo
+//
+//  Created by Yunus Emre BOLGÖNÜL on 22.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct FToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FToDoUITests/FToDoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  FToDoUITestsLaunchTests.swift
+//  FToDoUITests
+//
+//  Created by Yunus Emre BOLGÖNÜL on 22.08.2023.
+//
+
+import XCTest
+
+final class FToDoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
